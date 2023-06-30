7754da63b43c836d67f813520d6bec571e2e19b0
mwagner <mwagner@students.42wolfsburg.de>
2023-06-30 12:33:52 +0200

    cleanup

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -10,9 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
-#include "ScavTrap.hpp"
-#include "FragTrap.hpp"
 #include "DiamondTrap.hpp"
 
 int main()
